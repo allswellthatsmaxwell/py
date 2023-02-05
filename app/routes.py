b281d6ac474077d0b96064cf34a4581a859a754b
Maxwell <allswellthatsmaxwell9@gmail.com>
2023-02-05 15:13:36 -0600

    update

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,13 @@ import filesystem
 app = Flask(__name__)
 app_routes = Blueprint("app_routes", __name__)
 
-filesystem = filesystem.FileSystem("/mnt/c/Users/allsw/projects/structured-voice-logging/dev_app_data")
+filesystem = filesystem.FileSystem(root="/home/spherecatcher/structured-voice-logging/dev_app_data")
 
  
 @app_routes.route("/upload", methods=["POST"])
 def recording():
     # saves an audio file to the filesystem, returns the filename
+    print("Entering routes.recording...")
     audio_data = request.data
     
     app.logger.info(audio_data)
@@ -23,5 +24,5 @@ def recording():
     filesystem.save(destpath, audio_data)
     # transcription = svl.Transcriber()
     response = make_response(destpath)
-    response.headers['Permissions-Policy'] = 'microphone=(self)'
+    # response.headers['Permissions-Policy'] = 'microphone=(self)'
     return response
