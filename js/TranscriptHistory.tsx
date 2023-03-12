144dcb3e8f101f5ea8896572865865137a309f78
Maxwell <allswellthatsmaxwell9@gmail.com>
2023-03-12 05:58:46 -0500

    wrote history file first try

---
+++ js/TranscriptHistory.tsx
@@ -0,0 +1,94 @@
+import React, {useEffect} from 'react';
+import {View, Button, TextInput, FlatList, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import * as firebase from 'firebase';
+
+import {getStyles} from './styles';
+
+const projectStyles = getStyles();
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 2,
+    backgroundColor: "white",
+    width: "100%",
+    flexWrap: "wrap",
+    borderBottomWidth: 1,
+  },
+  mainTextContainer: {
+    backgroundColor: "#FFFFFF",
+    justifyContent: "center",
+    // alignSelf: "flex-start",
+    maxWidth: "100%",
+    width: 400,
+    height: 600,
+    maxHeight: 600,
+    // padding: 10,
+    paddingHorizontal: 10,
+    marginTop: 0,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#000000",
+    overflow: "hidden",
+  },
+  cell: {
+    flex: 1,
+    alignItems: "center",
+  }
+});
+
+export function TranscriptHistory({userId, fbase}) {
+
+  const [transcriptsList, setTranscriptsList] = React.useState([]);
+
+  useEffect(() => {
+    const transcriptsCollection = fbase
+        .firestore()
+        .collection("users")
+        .doc(userId)
+        .collection("transcripts");
+
+    const unsubscribe = transcriptsCollection.onSnapshot((snapshot) => {
+      const transcripts = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+      transcripts.sort((a, b) => b.timestamp - a.timestamp);
+      console.log("Logs: ", transcripts);
+      setTranscriptsList(transcripts);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [userId]);
+
+  const tableData = transcriptsList.map((record) => ({
+    timestamp: record.timestamp,
+    text: record.text,
+    topics: record.topics,
+    id: record.id
+  }));
+
+  return (
+      <View style={projectStyles.topContainer}>
+        <View style={[styles.mainTextContainer, {alignSelf: "flex-end"}]}>
+          <FlatList
+              data={tableData}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({item}) => (
+                  <View style={styles.row}>
+                    <Text style={[styles.cell, {textAlign: "left"}]}>
+                      {item.text}
+                    </Text>
+                    <Text style={[styles.cell, {textAlign: "center"}]}>
+                      {item.topics}
+                    </Text>
+                    <Text style={[styles.cell, {textAlign: "right"}]}>
+                      {item.timestamp}
+                    </Text>
+                  </View>
+              )}
+          />
+        </View>
+      </View>
+  );
+}
