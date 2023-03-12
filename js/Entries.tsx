99bd6cc1d420901272c88c7535a34148e95127b3
Maxwell <allswellthatsmaxwell9@gmail.com>
2023-03-11 21:58:12 -0600

    got slider visual element

--- js/Entries.tsx
+++ js/Entries.tsx
@@ -1,22 +1,40 @@
-import {View} from "react-native";
+import {TouchableOpacity, View, Animated, FlatList, Text, StyleSheet} from "react-native";
 import * as React from "react";
 import {useEffect} from "react";
 import * as firebase from "firebase";
 import {Table, Row, Rows} from "react-native-table-component";
+import {Swipeable} from "react-native-gesture-handler";
 
-import { getStyles } from "./styles";
+import {getStyles} from "./styles";
 
-const styles = getStyles();
+const projectStyles = getStyles();
 
-export function EntriesForTopic({ userId, selectedTopic }) {
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    width: 300,
+    borderWidth: 1,
+    borderColor: "#bbb"
+  },
+  header: {
+    backgroundColor: "#f1f8ff",
+    flexDirection: "row"
+  },
+  cell: {
+    margin: 1,
+    flex: 1
+  }
+});
+
+export function EntriesForTopic({userId, selectedTopic}) {
   return (
-      <View style={styles.topContainer}>
-        <LogsList userId={userId} topic={selectedTopic} />
+      <View style={projectStyles.topContainer}>
+        <LogsList userId={userId} topic={selectedTopic}/>
       </View>
   );
 }
 
-function LogsList({ userId, topic }) {
+function LogsList({userId, topic}) {
   const [logsList, setLogsList] = React.useState([]);
 
   useEffect(() => {
@@ -29,7 +47,8 @@ function LogsList({ userId, topic }) {
         .collection("entries");
 
     const unsubscribe = logsCollection.onSnapshot((snapshot) => {
-      const logs = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const logs = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+      logs.sort((a, b) => b.timestamp - a.timestamp);
       console.log("Logs: ", logs);
       setLogsList(logs);
     });
@@ -39,10 +58,66 @@ function LogsList({ userId, topic }) {
     };
   }, [userId, topic]);
 
+
+  const handleDelete = (log) => {
+    // Delete the log from the database
+    firebase
+        .firestore()
+        .collection("users")
+        .doc(userId)
+        .collection("topics")
+        .doc(topic)
+        .collection("entries")
+        .doc(log.id)
+        .delete()
+        .then(() => {
+          console.log("Log deleted successfully!");
+        })
+        .catch((error) => {
+          console.error("Error deleting log: ", error);
+        });
+  };
+
+  const renderRightActions = (progress, dragX, log) => {
+    const trans = dragX.interpolate({
+      inputRange: [0, 50, 100],
+      outputRange: [-20, 0, 100],
+    });
+
+    return (
+        <TouchableOpacity
+            style={{
+              backgroundColor: "red",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              paddingHorizontal: 20,
+              height: 70
+            }}
+            onPress={() => handleDelete(log)}
+        >
+          <Animated.Text style={{transform: [{translateX: trans}], color: "#000000", fontWeight: "bold"}}>
+            delete
+          </Animated.Text>
+        </TouchableOpacity>
+    );
+  };
+
+  const renderItem = ({item}) => {
+    return (
+        <Swipeable renderRightActions={(progress, dragX) => renderRightActions(progress, dragX, item)}>
+          <View style={styles.header}>
+            {item.map((cell) => (
+                <Text style={styles.cell}>{cell}</Text>
+            ))}
+          </View>
+        </Swipeable>
+    );
+  };
+
   const tableHead = ["Date", "Time", "Value"];
   // formats like "July 6, 11:36 PM"
-  const time_format = { hour: "2-digit", minute: "2-digit", hour12: true };
-  const day_format = { month: "short", day: "numeric" };
+  const time_format = {hour: "2-digit", minute: "2-digit", hour12: true};
+  const day_format = {month: "short", day: "numeric"};
   const tableData = logsList.map((log) => [
     log.timestamp.toDate().toLocaleDateString([], day_format),
     log.timestamp.toDate().toLocaleTimeString([], time_format),
@@ -50,18 +125,17 @@ function LogsList({ userId, topic }) {
   ]);
 
   return (
-      <View style={{ height: 100, width: 300 }}>
-        <Table
-            style={{ width: "100%" }}
-            borderStyle={{ borderWidth: 1, borderColor: "#bbb" }}
-        >
-          <Row
-              data={tableHead}
-              style={{ backgroundColor: "#f1f8ff" }}
-              textStyle={{ margin: 1 }}
-          />
-          <Rows data={tableData} textStyle={{ margin: 1 }} />
-        </Table>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          {tableHead.map((item) => (
+              <Text style={styles.cell}>{item}</Text>
+          ))}
+        </View>
+        <FlatList
+            data={tableData}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+        />
       </View>
   );
 }
