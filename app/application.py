419951e4788415663e2b9e3d87658bfa1eda032d
Maxwell <allswellthatsmaxwell9@gmail.com>
2023-03-15 00:35:55 -0500

    update

--- app/application.py
+++ app/application.py
@@ -5,9 +5,6 @@ def create_app():
     app = Flask(__name__)
     app.config['CORS_HEADERS'] = 'Content-Type'
     
-    from .routes import app_routes
-    app.register_blueprint(app_routes)
-    
     CORS(app)
     
     return app
