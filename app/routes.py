e5bbd6548e27a03b940d26a7303601096958dae3
Maxwell <allswellthatsmaxwell9@gmail.com>
2023-03-15 00:29:24 -0500

    many deletes

--- app/routes.py
+++ app/routes.py
@@ -18,24 +18,11 @@ transcriber = transcription.WhisperTranscriber()
 
 @app_routes.route("/transcribe", methods=["POST"])
 async def transcribe():
-    # saves an audio file to the filesystem, returns the transcription ID
     print("Entering routes.transcribe...")
     
-    # print(f"request.form: {request.form}")
-    # print(f"request.files: {request.files}")
-    # if 'file' not in request.files:
-    #     return "No file found in the request", 400
-
-    # # Get the audio file from the request
-    # audio_file = request.files['file']
-    # print("/transcribe: audio_file:", audio_file)
-    # # print("/transcribe: audio_file.filename:", audio_file.filename)
     audio_data = request.get_data()
 
-    # audio_data = audio_file.read()
     print("/transcribe: len(audio_data):", len(audio_data))
-    # extension = audio_file.filename.split(".")[-1]
-    # app.logger.info(f"extension: {extension}")
     
     dest_dir = os.path.join(filesystem.root, "recordings")
     os.makedirs(dest_dir, exist_ok=True)
@@ -54,27 +41,3 @@ async def transcribe():
     return make_response(jsonify(response_data))
 
 
-# @app_routes.route("/kickoff", methods=["POST"])
-# def kickoff():
-#     print(request)
-#     print(request.get_json())
-#     audio_url = request.get_json()['audio_url']
-#     return make_response(jsonify(transcription.kickoff(audio_url)))
-    
-
-
-# # an endpoint that takes a transcript and returns a list of topics
-# @app_routes.route("/topics", methods=["POST"])
-# def topics():
-#     transcript = request.get_json()
-#     text = transcript['text']
-#     file_finder = LogFilesFinder(text, LOGFILES_DIR)
-#     topics = {'topics': file_finder.relevant_files}
-#     return make_response(topics)
-
-
-# def transcribe(audio_file: str) -> Dict:
-#     transcriber = transcription.Transcriber(audio_dir=f"{filesystem.root}/recordings")
-#     transcript = transcriber.transcribe(audio_file)
-#     app.logger.info(f'transcript: "{transcript}"')
-#     return transcript
