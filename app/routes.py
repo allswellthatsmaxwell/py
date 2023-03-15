5cba63231ebe3aafd5010a61dae6dba6c97b2fbb
Maxwell <allswellthatsmaxwell9@gmail.com>
2023-03-14 22:37:00 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,7 @@ async def transcribe():
     app.logger.info("Done writing.")
     
     app.logger.info("Transcribing...")
-    transcript = await transcriber.transcribe(destpath)
+    transcript = await transcriber.transcribe(audio_file)
     app.logger.info("Done transcribing.")
     print(transcript)
     response_data = {'transcription': transcript}
